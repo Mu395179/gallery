b134d18440d9cbe9a924ab90888f2428fc23d8d4
Mu395179 <d128579744@gmail.com>
2024-06-05 09:01:27 +0800

    建立簡單index和上傳作品的upload頁面

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="upload.php">表單檔案上傳</a>
+    <a href="manage.php">檔案管理</a>
+
+</body>
+
+</html>
