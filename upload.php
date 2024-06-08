9b270184a3f011f53275a13511d30f6c7997c284
Mu395179 <d128579744@gmail.com>
2024-06-09 00:09:35 +0800

    在upload.php 增加bootstrap表單排版，增加預覽圖片的card排版

--- upload.php
+++ upload.php
@@ -56,39 +56,72 @@ if (!empty($_FILES)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        form {
+            background-color: lightgray;
+            border: 1px black solid;
+        }
+
+        .form-control {
+            border: 1px solid black;
+            
+        }
+    </style>
 </head>
 
 <body>
     <h1 class="header">檔案上傳</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <label class="form-label mt-2" for="name">上傳檔案:</label>
+                    <input class="form-control " type="file" name="file">
+                    <label class="form-label" for="name">作品名稱:</label>
+                    <input class="form-control " type="text" name="name">
+                    <label class="form-label" for="description">描述:</label>
+                    <textarea class="form-control " type="text" name="description" rows="5" cols="100"></textarea>
+                    <label class="form-label" for="style">直幅/橫幅</label>
+                    <select class="form-control " type="text" name="style">
+                        <?php
+                        $styles = $pdo->query('select * from style')->fetchAll();
+                        foreach ($styles as $style) {
+                            echo "<option value='{$style['id']}'>{$style['ch_name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <input class="mt-5" type="submit" value="上傳">
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <label for="name">作品名稱:</label>
-        <input type="text" name="name">
-        <label for="description">描述:</label>
-        <textarea type="text" name="description" rows="10" cols="100"></textarea>
-        <label for="style">直幅/橫幅</label>
-        <select type="text" name="style">
+                </form>
+            </div>
+
+            <!-- 建立一個連結來查看上傳後的圖檔 -->
             <?php
-            $styles = $pdo->query('select * from style')->fetchAll();
-            foreach ($styles as $style) {
-                echo "<option value='{$style['id']}'>{$style['ch_name']}</option>";
-            }
-            ?>
+            $images = all('text');
 
-        </select>
-        <input type="submit" value="上傳">
+            foreach ($images as $image) {
+                ?>
+                <div class="col-6">
 
-    </form>
-    <!-- 建立一個連結來查看上傳後的圖檔 -->
-
+                    <div class="card" style="width:400px">
+                        <img class="card-img-top" <?= "src='images/{$image['file_name']}'" ?> alt="Card image"
+                            style="width:100%">
+                        <div class="card-body">
+                            <h4 class="card-title"><?= "{$image['original_name']}" ?></h4>
+                            <p class="card-text"><?= "{$image['description']}" ?></p>
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
     <h1>直幅</h1>
     <?php
 
-    $images = all('text',"WHERE style = 1");
-
-dd($images);
-
+    $images = all('text', "WHERE style = 1");
     foreach ($images as $image) {
         echo "<div class='upload-img'>";
         echo "<a class='pen' href='edit_image.php?id={$image['id']}'>";
@@ -100,20 +133,17 @@ dd($images);
     }
     ?>
     <h1>橫幅</h1>
-<?php
-    $images2 = all('text',"WHERE style = 2");
-
-    dd($images2);
-    
-        foreach ($images2 as $image2) {
-            echo "<div class='upload-img'>";
-            echo "<a class='pen' href='edit_image.php?id={$image2['id']}'>";
-            echo "<img src='./pen.png' style='width:15px;height:15px;'>";
-            echo "</a>";
-            echo "<a class='del' href='del_image.php?id={$image2['id']}'>X</a>";
-            echo "<img src='images/{$image2['file_name']}'>";
-            echo "</div>";
-        }
+    <?php
+    $images2 = all('text', "WHERE style = 2");
+    foreach ($images2 as $image2) {
+        echo "<div class='upload-img'>";
+        echo "<a class='pen' href='edit_image.php?id={$image2['id']}'>";
+        echo "<img src='./pen.png' style='width:15px;height:15px;'>";
+        echo "</a>";
+        echo "<a class='del' href='del_image.php?id={$image2['id']}'>X</a>";
+        echo "<img src='images/{$image2['file_name']}'>";
+        echo "</div>";
+    }
 
     ?>
 
