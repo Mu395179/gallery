5b7cea766ee2b78ab5cc27c0cfa271a7c12c477f
Mu395179 <d128579744@gmail.com>
2024-06-09 18:26:20 +0800

    修正edit_image.php的排版錯位問題

--- edit_image.php
+++ edit_image.php
@@ -45,78 +45,114 @@ if (!empty($_FILES)) {
         echo "檔案上傳失敗";
     }
 }
+
 ?>
-<h1 class="header">編輯檔案上傳</h1>
-<div class="container d-flex justify-content-center">
-    <div class="row">
-        <div class="col-6">
-            <form action="edit_image.php?id=<?= $_GET['id']; ?>" method="post" enctype="multipart/form-data">
-                <label class="form-label mt-2" for="name">上傳檔案:</label>
-                <input class="form-control " type="file" name="file">
-                <label class="form-label mt-2" for="name">作品名稱:</label>
-                <input class="form-control " type="text" name="name">
-                <label class="form-label mt-2" for="description">描述:</label>
-                <textarea class="form-control " type="text" name="description" rows="5" cols="100"></textarea>
-                <label class="form-label mt-2" for="style">直幅/橫幅:</label>
-                <select class="form-control " type="text" name="style">
-                    <?php
-                    $styles = $pdo->query('select * from style')->fetchAll();
-                    foreach ($styles as $style) {
-                        echo "<option value='{$style['id']}'>{$style['ch_name']}</option>";
-                    }
-                    ?>
-                </select>
-                <label class="form-label mt-2" for="method">手法:</label>
-                <select class="form-control " type="text" name="method">
-                    <?php
-                    $methods = $pdo->query('select * from method')->fetchAll();
-                    foreach ($methods as $method) {
-                        echo "<option value='{$method['id']}'>{$method['ch_name']}</option>";
-                    }
-                    ?>
-                </select>
-                <label class="form-label mt-2" for="purpose">分類:</label>
-                <select class="form-control " type="text" name="purpose">
-                    <?php
-                    $purposes = $pdo->query('select * from purpose')->fetchAll();
-                    foreach ($purposes as $purpose) {
-                        echo "<option value='{$purpose['id']}'>{$purpose['ch_name']}</option>";
-                    }
-                    ?>
-                </select>
-                <label class="form-label mt-2" for="size">尺寸:</label>
-                <select class="form-control " type="text" name="size">
-                    <?php
-                    $sizes = $pdo->query('select * from size')->fetchAll();
-                    foreach ($sizes as $size) {
-                        echo "<option value='{$msize['id']}'>{$size['name']}</option>";
-                    }
-                    ?>
-                </select>
-                <input class="mt-5" type="submit" value="上傳">
+<!DOCTYPE html>
+<html lang="en">
 
-            </form>
-        </div>
-    </div>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
-<?php
-$images = all('text');
+    <h1 class="header">編輯檔案上傳</h1>
+    <div class="container d-flex justify-content-center">
+        <div class="row">
+            <div class="col-6">
+                <form action="edit_image.php?id=<?= $_GET['id']; ?>" method="post" enctype="multipart/form-data">
+                    <label class="form-label mt-2" for="name">上傳檔案:</label>
+                    <input class="form-control " type="file" name="file">
+                    <label class="form-label mt-2" for="name">作品名稱:</label>
+                    <input class="form-control " type="text" name="name">
+                    <label class="form-label mt-2" for="description">描述:</label>
+                    <textarea class="form-control " type="text" name="description" rows="5" cols="100"></textarea>
+                    <label class="form-label mt-2" for="style">直幅/橫幅:</label>
+                    <select class="form-control " type="text" name="style">
+                        <?php
+                        $styles = $pdo->query('select * from style')->fetchAll();
+                        foreach ($styles as $style) {
+                            echo "<option value='{$style['id']}'>{$style['ch_name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <label class="form-label mt-2" for="method">手法:</label>
+                    <select class="form-control " type="text" name="method">
+                        <?php
+                        $methods = $pdo->query('select * from method')->fetchAll();
+                        foreach ($methods as $method) {
+                            echo "<option value='{$method['id']}'>{$method['ch_name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <label class="form-label mt-2" for="purpose">分類:</label>
+                    <select class="form-control " type="text" name="purpose">
+                        <?php
+                        $purposes = $pdo->query('select * from purpose')->fetchAll();
+                        foreach ($purposes as $purpose) {
+                            echo "<option value='{$purpose['id']}'>{$purpose['ch_name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <label class="form-label mt-2" for="size">尺寸:</label>
+                    <select class="form-control " type="text" name="size">
+                        <?php
+                        $sizes = $pdo->query('select * from size')->fetchAll();
+                        foreach ($sizes as $size) {
+                            echo "<option value='{$msize['id']}'>{$size['name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <input class="mt-5" type="submit" value="上傳">
 
-foreach ($images as $image) {
-    ?>
-    <div class="col-6">
-
-        <div class="card" style="width:400px">
-            <img class="card-img-top" <?= "src='images/{$image['file_name']}'" ?> alt="Card image" style="width:100%">
-            <div class="card-body">
-                <h4 class="card-title"><?= "{$image['original_name']}" ?></h4>
-                <p class="card-text"><?= "{$image['method']}" ?></p>
-                <p class="card-text"><?= "{$image['purpose']}" ?></p>
-                <p class="card-text"><?= "{$image['size']}" ?></p>
-                <p class="card-text"><?= "{$image['description']}" ?></p>
-                <a href="#" class="btn btn-primary">See Profile</a>
+                </form>
             </div>
+
         </div>
-    </div>
-<?php } ?>
+
+        <?php
+        $images = all('text');
+
+        foreach ($images as $image) {
+            ?>
+            <div class="col-5 mt-5">
+
+                <?php foreach ($images as $index => $image) { ?>
+                    <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
+
+                        <div class="card m-auto mt-3" style="width:400px">
+                            <img class="card-img-top" <?= "src='images/{$image['file_name']}'" ?> alt="Card image"
+                                style="width:100%">
+                            <div class="card-body">
+                                <h4 class="card-title"><?= "{$image['original_name']}" ?></h4>
+                                <p class="card-text"><?= "{$image['method']}" ?></p>
+                                <p class="card-text"><?= "{$image['purpose']}" ?></p>
+                                <p class="card-text"><?= "{$image['size']}" ?></p>
+                                <p class="card-text"><?= "{$image['description']}" ?></p>
+                                <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                            </div>
+                            <!-- <div class="row">
+                        <div class="col-3"></div>
+                        <div class="col-4">
+                            <a <?= "href='edit_image.php?id={$image['id']}'" ?> class="btn btn-primary">編輯</a>
+                        </div>
+                        <div class="col-4">
+                            <a <?= "href='del_image.php?id={$image['id']}'" ?> class="btn btn-primary">刪除</a>
+                        </div>
+                    </div> -->
+                        </div>
+
+                    </div>
+                <?php } ?>
+            </div>
+
+        </div>
+
+        
+
+
+
+    <?php } ?>
