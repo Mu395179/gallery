25dc4f3487f60db78008ee30dea285b1eed6ab36
Mu395179 <d128579744@gmail.com>
2024-06-23 08:03:26 +0800

    新增manage.php

--- api/get_text.php
+++ api/get_text.php
@@ -1,4 +1,4 @@
 <?php include_once "base.php";
 
-$text = q('select `classroom` from `students` group by `classroom`');
-echo json_encode($classrooms);
+$text = q("SELECT `text`.`id`,`file_name`, `original_name`,`description`,`purpose_ch_name`,`style_ch_name`,`size_name`,`method_ch_name` FROM `text` JOIN`purpose`ON`text`.`purpose` = `purpose`.`id` JOIN`style`ON`text`.`style` = `style`.`id` JOIN`size`ON`text`.`size` = `size`.`id` JOIN`method`ON`text`.`method` = `method`.`id` WHERE ");
+echo json_encode($text);
